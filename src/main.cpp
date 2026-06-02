70d18c98459f22cfd8731acec9a7f9bac9d034b6
Debabrata Batabyal <dbatabyal2018@gmail.com>
2026-06-02 18:54:08 +0530

    integrated ImGui and rlImGui for GUI support

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <raylib.h>
 
+#include <imgui.h>
+#include <rlImGui.h>
+
 int main()
 {
 	std::cout << "Hello\n";
@@ -8,6 +11,7 @@ int main()
 	SetTargetFPS(60);
 
 	InitWindow(800, 450, "game");
+	rlImGuiSetup(true);
 
 	int posX = 30;
 	int posY = 30;
@@ -17,13 +21,18 @@ int main()
 	{
 		BeginDrawing();
 		ClearBackground(BLACK);
+
+		rlImGuiBegin();
+		ImGui::ShowDemoWindow();
 		DrawText("Window", 190, 200, 20, {255, 0, 0, 127});
 		DrawRectangle(75, 75, 100, 100, {255, 0, 0, 127});
 		DrawRectangle(50, 50, 100, 100, {0, 255, 0, 127});
 
+		rlImGuiEnd();
 		EndDrawing();
 	}
 
+	rlImGuiShutdown();
 	CloseWindow();
 
 	return 0;
